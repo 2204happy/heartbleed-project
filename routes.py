ecf9da5b09b196db74255ba4c3db3d17d4a4439e
2204happy <benbartley2@gmail.com>
2019-05-27 09:15:33 +1000

    test

--- routes.py
+++ routes.py
@@ -1,7 +1,9 @@
-from . import app
-from flask import request, render_template
+from . import *
+from flask import *
 
 from datetime import *
+
+#url_for('static', filename='style.css')
 
 @app.route('/')
 @app.route('/index.html')
