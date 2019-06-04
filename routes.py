19e4b245373af8391f0f806df97ac93390526d93
Joseph-Davies <daviesjd1@gmail.com>
2019-06-04 10:20:25 +1000

    first intereactive bit

--- routes.py
+++ routes.py
@@ -18,5 +18,30 @@ def protocols():
 def style():
   return render_template("style.css")
 
-@app.route("/demonstration"):
-  return "hello"
+output = ""
+secret_string = "a97b79,password_user_bob=\"password12345\",cockie_data_session_1=\"bannana\",123f7a757575a57c57meaning_of_life=42,"
+
+@app.route("/demonstration")
+def demo():
+  global output
+  
+  message = str(request.args.get("request"))
+  try:
+    length = int(request.args.get("length"))
+  
+    if length <= len(message):
+      output = message[0:length]
+    else:
+      output = message
+      length -= len(message)
+      output += secret_string[0:length]
+      if length > len(secret_string):
+        length -= len(secret_string)
+        with open("bee_movie_script", 'r') as bee_movie_file:
+      	  output += bee_movie_file.read(length)
+  
+    #output = str(paramters.get("request")) + str(paramters.get("length"))
+  except ValueError:
+    output = "Error: input must be an integer"
+  
+  return "Input: <br> <form method=\"get\"> Request: <input type=\"text\" name=\"request\"> <br> Length of request: <input type=\"text\" name=\"length\"> <br> <input type=\"submit\"> </form> Output: <br>" + str(output)
